9ca75ac014ebbe7ed3083d5de054694aa5eef024
Hermet Michael <hermet.mkl@gmail.com>
2026-02-28 17:43:03 +0100

    wire i18n lang to the real html lang attribute

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,14 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+const updateHtmlLanguage = (language: string) => {
+  if (typeof document === "undefined") {
+    return;
+  }
+
+  document.documentElement.lang = language;
+};
+
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
@@ -27,5 +35,15 @@ i18n
       escapeValue: false,
     },
   });
+
+i18n.on("languageChanged", updateHtmlLanguage);
+
+if (i18n.isInitialized) {
+  updateHtmlLanguage(i18n.resolvedLanguage ?? i18n.language);
+} else {
+  i18n.on("initialized", () => {
+    updateHtmlLanguage(i18n.resolvedLanguage ?? i18n.language);
+  });
+}
 
 export default i18n;
